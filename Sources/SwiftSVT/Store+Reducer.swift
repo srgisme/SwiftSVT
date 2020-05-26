82c49445d548d5d138ca83952d7e1352a90f8650
Scott Gorden <srgisme@gmail.com>
2020-05-25 22:47:23 -0700

    - change StateChange to Reducer<State>

--- Sources/SwiftSVT/Store+Reducer.swift
+++ Sources/SwiftSVT/Store+Reducer.swift
@@ -8,10 +8,11 @@
 import Combine
 import Foundation
 
+/// `Reducer`s are publishers that publish closure elements that can modify your application's state. State changes should only happen here.
+public typealias Reducer<State> = AnyPublisher<(inout State) -> Void, Never>
+
 /// Your application should have only one `Store`. You can not subclass it, but it is generic over any app state type you want. Since the `Store` is an `ObservableObject`, any `View`s with an `@ObservedObject` (or `@EnvironmentObject`) property of this type will automatically be updated any time the `state` changes. If you want to prevent child `View`s from being updated upon `state` changes, wrap the parent `View` in an `EquatableView` (you can use the `equatable()` modifier on the parent as well).
 final public class Store<State>: ObservableObject {
-    /// A publisher that publishes a closure that takes a `StateType` as a parameter. Mutating the `Store`'s state should only happen inside this closure.
-    public typealias StateChange = AnyPublisher<(inout State) -> Void, Never>
     /// The store's state.
     @Published public private(set) var state: State
     private var subscriptions: Set<AnyCancellable> = []
@@ -20,13 +21,12 @@ final public class Store<State>: ObservableObject {
         self.state = initialState
     }
 
-    /// This method is used to submit a `StateChange` to the `Store` and subsequently mutate the store's state. When a new state change is sent, the store subscribes to its publisher pipeline. Since the values submitted to the store are simply publishers, you have a lot of flexibility to design your state change publishers as you see fit.
-    /// - Parameter stateChange: The state change to be applied to the `Store`'s state
-    /// - Important: You can only send state changes to the store when both the `Store`'s state and the state the transaction is mutating are of the same type.
-    public func send(_ stateChange: StateChange) {
-        stateChange
+    /// This method is used to submit a `Reducer` publisher to the `Store` that subsequently mutates the store's state. When a `Reducer` is sent, the store subscribes to its publisher pipeline. Since the values submitted to the store are simply publishers, you have a lot of flexibility to design your `Reducer` publishers as you see fit.
+    /// - Parameter reducer: The `Reducer` whose state changes are to be applied to the `Store`'s state
+    /// - Important: You can only send `Reducer`s to the store when both the `Store`'s state and the state the transaction is mutating are of the same type.
+    public func send<P: Publisher>(_ reducer: P) where P.Output == Reducer<State>.Output, P.Failure == Reducer<State>.Failure {
+        reducer
             .receive(on: DispatchQueue.main)
-            .print()
             .sink { [weak self] in
                 guard let self = self else { return }
                 $0(&self.state)
